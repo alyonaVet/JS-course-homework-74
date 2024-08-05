7f924c2250cdcb9b8c94a3c007323bcf96110e71
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-05 13:22:50 +0600

    added path messagesRouter;

--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import messagesRouter from "./routers/messages";
 
 const app = express();
 const port = 8000;
 
 app.use(express.json());
+app.use('/messages', messagesRouter);
 
 app.listen(port, () => {
     console.log(`Server at: http://localhost:${port}`);
