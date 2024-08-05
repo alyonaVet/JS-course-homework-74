e0c45d673a10a1c4764457a163f25b76fae8e524
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-05 18:38:51 +0600

    refactor messagesRouter.post; returned new message to message file;

--- routers/messages.ts
+++ routers/messages.ts
@@ -1,13 +1,27 @@
 import express from "express";
+import {promises as fs} from 'fs';
+import {IMessage} from "../types";
 
-const messagesRouter  = express.Router();
+
+const messagesRouter = express.Router();
 
 messagesRouter.get("/", (req, res) => {
+    let data: IMessage[] = [];
+
     return res.send("Here are your messages: ");
 });
 
-messagesRouter.post("/", (req, res) => {
-    return res.send(req.body);
+messagesRouter.post("/", async (req, res) => {
+    const datetime = new Date().toISOString();
+    const fileName = `${datetime}.txt`;
+
+    const newMessage: IMessage = {
+        message: req.body.message,
+        datetime,
+    };
+
+    await fs.writeFile(`./messages/${fileName}`, JSON.stringify(newMessage));
+    return res.send(newMessage);
 });
 
 export default messagesRouter;
