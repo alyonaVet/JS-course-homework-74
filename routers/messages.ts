87d4deb818b15e31b6d1ab74af20f78653b44a1b
alyonaVet <a.v.alenka2015@gmail.com>
2024-08-06 13:20:40 +0600

    created getFiles for file reading and get the last 5 messages; refactor messagesRouter.get; refactor messagesRouter.post;

--- routers/messages.ts
+++ routers/messages.ts
@@ -2,13 +2,34 @@ import express from "express";
 import {promises as fs} from 'fs';
 import {IMessage} from "../types";
 
+const path = './messages';
 
 const messagesRouter = express.Router();
 
-messagesRouter.get("/", (req, res) => {
-    let data: IMessage[] = [];
+const getFiles = async (fileList: string[]) => {
+    const data: IMessage[] = [];
+    for (const fileName of fileList) {
+        const filePath = `${path}/${fileName}`;
+        try {
+            const fileContent = await fs.readFile(filePath);
+            const dataItem = JSON.parse(fileContent.toString());
+            data.push(dataItem);
+        } catch (error) {
+            console.error('Failed to read file ', error);
+        }
+    }
+    return data.slice(-5);
+};
 
-    return res.send("Here are your messages: ");
+messagesRouter.get("/", async (req, res) => {
+    try {
+        const fileList = await fs.readdir(path);
+        const data = await getFiles(fileList)
+
+        return res.send(data);
+    } catch (error) {
+        console.log('Error reading ',error);
+    }
 });
 
 messagesRouter.post("/", async (req, res) => {
@@ -19,8 +40,11 @@ messagesRouter.post("/", async (req, res) => {
         message: req.body.message,
         datetime,
     };
-
-    await fs.writeFile(`./messages/${fileName}`, JSON.stringify(newMessage));
+    try {
+        await fs.writeFile(`${path}/${fileName}`, JSON.stringify(newMessage));
+    } catch (error) {
+        console.error('Failed to write message', error);
+    }
     return res.send(newMessage);
 });
 
